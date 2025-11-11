e77cc3556c89329d11dc3e48e950fb1fa333e45b
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-11-12 02:05:03 +0600

    add staticfiles to gitignore and config whitenoise

--- backend/project/config/base.py
+++ backend/project/config/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
--- backend/project/config/production.py
+++ backend/project/config/production.py
@@ -21,6 +21,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
